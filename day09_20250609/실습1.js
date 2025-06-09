f2afaa070999448bd2bff922f1a6bb2e003a76de
leechanyeong <yahn10@naver.com>
2025-06-09 13:05:19 +0900

    js 중간 저장

---
+++ day09_20250609/실습1.js
@@ -0,0 +1,19 @@
+console.log("숫자 자료형")
+console.log("대기번호",100)
+console.log("수량",1,"ea")
+console.log("유료 옵션추가","샷추가","2")
+
+console.log("문자열 자료형")
+console.log("주문금액")
+console.log("할인금액")
+console.log("결제금액")
+
+console.log("불리언 자료형")
+console.log("쿠폰적립")
+console.log("유료옵션")
+console.log("음료 주문후 선택완료or취소")
+
+console.log("배열 자료형")
+console.log("카테고리종류")
+console.log("카테고리 음료")
+console.log("결제하기")
