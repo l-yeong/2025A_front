3902c8d5af88c803522acdeb3926cc306d2b1973
leechanyeong <yahn10@naver.com>
2025-06-19 10:26:45 +0900

    실습11_5,7번 복습

--- day16_20250618/실습11.js
+++ day16_20250618/실습11.js
@@ -67,6 +67,12 @@ console.log(loseScore())
 console.log(userScore)
 // 실습 5: 최고 점수 학생 찾기
 // 전역변수 students 배열 데이터를 이용하여 , 가장 높은 점수(score)를 가진 학생의 이름을 찾아 반환하는 findTopStudent 함수를 만드세요.
+//1. 매개변수:x
+//2. 리턴: 가장높은점수
+//3. 로직:가장 높은 점수(score)를 가진 학생의 이름을 찾는행동
+//
+
+
 const students = [
   { name: '김철수', score: 85 },
   { name: '이영희', score: 92 },
@@ -75,15 +81,38 @@ const students = [
 
 
 function findTopStudent (){ 
-    for(i=0; i<=students.length -1; i++){
+    let maxNumber=students[0];
+    for(i=0; i<= students.length -1; i++){
         let scoreMax=students[i]
-        if(scoreMax.score>max){
-            console.log(scoreMax)
+        if(scoreMax.score>maxNumber){
+            maxNumber=scoreMax.score;
+            
         }
         
     }
 }
-findTopStudent()
+let topStudent=findTopStudent()
+console.log(topStudent)
+
+
+
+
+// function findTopStudent(  ){
+//     let maxObj = { name : '' , score : 0 }
+//     for( let index = 0 ; index <= students.length - 1 ; index++ ){
+//         const student =  students[index];
+//         if( student.score > maxObj.score ){
+//             maxObj = student;    
+//         }
+//     } // for end 
+//     return maxObj.name; // 가장높은점수의이름
+// } // f end 
+// // 확인차
+// let result = findTopStudent(); 
+// console.log( result );
+
+
+
 
 // 실습 6: 상품 목록 페이지 만들기 (참고: 예제 6, 7, 8, 12)
 // 전역변수 products 배열 데이터를 이용하여, 각 상품을 소개하는 HTML 요소를 동적으로 만들어 페이지에 표시하는 renderProducts 함수를 만드세요.
@@ -94,9 +123,47 @@ findTopStudent()
 //   { name: '키보드', price: 80000, stock: 25 }
 // ];
 
+// function renderProducts(){
+//     let html=`<div>`
+//     for(i=0; i<=products.length-1; i++){
+//         let product=products[i];
+//         console.log(product)
+//         html+=`<h4> 상품:${product.name}</h4>
+//          <p>  가격:${product.price}</p>
+//          <p>  재고:${product.stock}</p>
+//          </div>
+//         `
+//     }document.write(html);
+// }
+// renderProducts();
+
+
 // 실습 7: 간단한 계산기
 // num1, num2, operator ( '+', '-') 세 개의 매개변수를 받는 calculator 함수를 만드시오.
 // operator가 '+'이면 두 수의 합을, '-'이면 두 수의 차를 콘솔에 출력하시오. (if문 사용)
+
+// function calculator(num1,num2,operator){
+//     const number1 = prompt("숫자를 입력하세요");
+//     const operator = prompt("연산자를 입력하세요");
+//     const number2 = prompt("숫자를 입력하세요");
+//     let result;
+//     if(operator === "+"){
+//         result = number1 + number2;
+//     }else if(operator === "-"){
+//         result = number1 - number2;
+//     }else if(operator === "*"){
+//         result = number1 * number2;
+//     }else if(operator === "/"){
+//         result = number1 / number2;
+//     }else{
+//         alert("연산자를 잘못 입력하셨습니다.");
+//     }
+//     console.log(result);
+//     return result;
+// }
+
+
+// calculator();
 
 
 // 실습 8: HTML 리스트(ul, li) 동적 생성
@@ -105,9 +172,34 @@ findTopStudent()
 // 최종적으로 이 문자열을 HTML <ul> 태그로 표현하여 리스트를 화면에 그리시오.
 // let todoList = ['장보기', '운동하기']
 
+// function renderList(){
+//     let html='';
+//     for(i=0;i<=todoList.length-1; i++){
+//         let todo=todoList[i]
+//         html+=`<ul>
+//                 <li>${todo}</li>
+//                 </ul>`
+//     }
+//     document.write(html)
+// }
+// renderList();
+
 // 실습 9: 입장료 계산 함수
 // 임의의 나이(age)를 매개변수로 받아 입장료를 반환하는 getTicketPrice 함수를 만드시오.
 // 8세 미만: "무료"
 // 8세 이상 19세 이하: "5,000원"
 // 20세 이상: "10,000원"
 // 계산된 가격 문자열을 반환(return)하시오.
+
+
+// function getTicketPrice(n){
+//     if(n>=20){
+//         return "10,000원";
+//     }else if(n>=8){
+//         return "5,000원";
+//     }else{return "무료";}
+
+// }
+
+// let age= getTicketPrice(8);
+// console.log(age);
